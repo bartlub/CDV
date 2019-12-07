b2ff9809d8a2d60a70dc314e3859c54ab4010e72
= <=>
2019-12-07 16:06:10 +0100

    update

--- PycharmProjects/Test/test_check.py
+++ PycharmProjects/Test/test_check.py
@@ -4,14 +4,17 @@ class MyTestCase(unittest.TestCase):
 
     def setUp(self):
         # przygotowanie do testu / warunki wstępne
+        print("Test przygotowany")
         pass
 
     def testGoogleSearch(self):
         # właściwy test
+        print("Test wykonany")
         pass
 
     def tearDown(self):
         # sprzątanie po teście
+        print("Posprzątane")
         pass
 
     if (__name__ == '__main__'):
